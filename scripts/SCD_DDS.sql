7c5c4a11c64f586d442279efb4dbe44d5d058043
lemin99 <leminh1031999@gmail.com>
2021-12-27 16:11:19 +0700

    + stage to DDS

--- scripts/SCD_DDS.sql
+++ scripts/SCD_DDS.sql
@@ -5,12 +5,55 @@ use SCD_DDS
 go
 
 create table DimCustomer (
-	CustomerKey int,
+	CustomerKey int IDENTITY(1,1),
 	CustomerID int,
 	CustomerName varchar(50),
 	CustomerSex varchar(50),
 	CustomerEmail varchar(50),
 	CustomerCity varchar(50),
+	CustomerJob varchar(50),
+	ValidFrom Date,
+	ValidTo Date,
+	[CustomerCompanyName] varchar(50),
+	[CustomerCorporateDepartment] varchar(50),
+	[CustomerRetailDepartment] varchar(50),
+	[CustomerJobSkill] varchar(50),
+	[CustomerStockSector] varchar(50),
+	[CustomerDateofBirth] Date,
+	[CustomerUniversityName] varchar(50),
+	[CustomerSchoolSubjects] varchar(50),
+	[CustomerEducationalinstitution] varchar(50),
+	[CustomerFavoriteMovieGenre] varchar(50),
+	[CustomerFavoriteMovie] varchar(50),
+	[CustomerFavoriteBookAuthors] varchar(50),
+	[CustomerfavoriteBook] varchar(50),
+	[CustomerfavoriteMusicGenre] varchar(50),
+	[CustomerfavoriteSong] varchar(50),
+	[CustomerCreditCardType] varchar(50),
+	[CustomerCreditCardNumber] varchar(50),
+	[CustomerCurrency] varchar(50),
+	[CustomerCountry] varchar(50),
+	[CustomerUSState] varchar(50),
+	[CustomerStreetAddress] varchar(200),
+	[CustomerShippingAddress] varchar(500),
+	[CustomerBussinessAddress] varchar(200),
+	[CustomerLanguage] varchar(50),
+	[CustomerStreet] varchar(50),
+	[CustomerClothesSize] varchar(50),
+	[CustomerCar] varchar(50),
+	[CustomerPersonalIdentificationNumber] varchar(50),
+	[CustomerPhoneNumber] varchar(50),
+	[CustomerIPv4] varchar(50),
+	[CustomerMaritalStatus] varchar(50),
+	[CustomerDriverLicense] varchar(50),
+	[CustomerNote] varchar(50)
 	primary key (CustomerKey)
 )
 go
+CREATE INDEX CustomerID_index
+ON DimCustomer (CustomerID);
+go
+Alter Table [dbo].[DimCustomer]
+ADD 
+[Hash_SCD1] AS CONVERT(CHAR(100),HASHBYTES('SHA2_256',CONCAT([CustomerName],[CustomerEmail],[CustomerDriverLicense],[CustomerNote],CustomerSex,[CustomerCar],[CustomerMaritalStatus],[CustomerIPv4],[CustomerPersonalIdentificationNumber],[CustomerPhoneNumber],[CustomerStockSector],[CustomerStreet],[CustomerLanguage],[CustomerFavoriteMovie],[CustomerfavoriteBook],[CustomerfavoriteSong],[CustomerCreditCardType],[CustomerClothesSize],[CustomerCreditCardNumber],[CustomerCurrency])),1),
+[Hash_SCD2] AS CONVERT(CHAR(100),HASHBYTES('SHA2_256',CONCAT(CustomerCity,[CustomerJob],[CustomerBussinessAddress],[CustomerCompanyName],[CustomerStreetAddress],[CustomerCorporateDepartment],[CustomerRetailDepartment],[CustomerJobSkill],[CustomerUniversityName],[CustomerSchoolSubjects],[CustomerEducationalInstitution],[CustomerFavoriteMovieGenre],[CustomerFavoriteBookAuthors],[CustomerfavoriteMusicGenre],[CustomerUSState],[CustomerCountry])),1);
--- scripts/SCD_Stage.sql
+++ scripts/SCD_Stage.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE [SCD_Stage]
 GO
-
 USE [SCD_Stage]
 GO
 
@@ -17,7 +16,40 @@ CREATE TABLE [dbo].[Customers](
 	[Name] [varchar](50) NULL,
 	[Sex] [varchar](1) NULL,
 	[City] [varchar](50) NULL,
-	[ID] [int] NULL
+	[ID] [int] IDENTITY(1,1) PRIMARY Key,
+	[Company Name] [varchar](50) NULL,
+	[Corporate Department] [varchar](50) NULL,
+	[Retail Department] [varchar](50) NULL,
+	[Job Skill] [varchar](50) NULL,
+	[Stock Sector] [varchar](50) NULL,
+	[Date of Birth] Date,
+	[University Name] [varchar](50) NULL,
+	[School Subjects] [varchar](50) NULL,
+	[Educational institution] [varchar](50) NULL,
+	[Favorite Movie Genre] [varchar](50) NULL,
+	[Favorite Movie] [varchar](50) NULL,
+	[Favorite Book Authors] [varchar](50) NULL,
+	[favorite Book] [varchar](50) NULL,
+	[favorite Music Genre] [varchar](50) NULL,
+	[favorite Song] [varchar](50) NULL,
+	[Credit Card Type] [varchar](50) NULL,
+	[Credit Card Number] [varchar](50) NULL,
+	[Currency] [varchar](50) NULL,
+	[Country] [varchar](50) NULL,
+	[US State] [varchar](50) NULL,
+	[Street Address] [varchar](200) NULL,
+	[Shipping Address] [varchar](500) NULL,
+	[Bussiness Address] [varchar](200) NULL,
+	[Language] [varchar](50) NULL,
+	[Street] [varchar](50) NULL,
+	[Clothes size] [varchar](50) NULL,
+	[Car] [varchar](50) NULL,
+	[Personal Identification Number] [varchar](50) NULL,
+	[Phone Number] [varchar](50) NULL,
+	[IPv4] [varchar](50) NULL,
+	[Marital Status] [varchar](50) NULL,
+	[Driver License] [varchar](50) NULL,
+	[Note] [varchar](50) NULL
 ) ON [PRIMARY]
 GO
 
